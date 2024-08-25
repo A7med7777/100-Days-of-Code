3972a837db7d77179e31fd6954e59c687fe185f1
A7med7777 <ahmedhessian56@gmail.com>
2024-08-25 16:47:20 +0300

    Day 30 - Intermediate - Errors, Exceptions and JSON Data: Improving the Password

--- Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/main.py
+++ Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/main.py
@@ -1,7 +1,54 @@
-from tkinter import *
+from cryptography.fernet import Fernet
 from tkinter import messagebox
+from tkinter import *
 import pyperclip
 import random
+import json
+import os
+
+
+# ---------------------------- SECURITY SETUP ------------------------------- #
+def load_or_generate_key():
+    if not os.path.exists("key.key"):
+        key = Fernet.generate_key()
+
+        with open("key.key", "wb") as file:
+            file.write(key)
+    else:
+        with open("key.key", "rb") as file:
+            key = file.read()
+
+    return Fernet(key)
+
+
+cipher = load_or_generate_key()
+
+
+def encrypt_data(data):
+    return cipher.encrypt(data.encode()).decode()
+
+
+def decrypt_data(data):
+    return cipher.decrypt(data.encode()).decode()
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+
+    if website:
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+
+            email = decrypt_data(data[website]["email"])
+            password = decrypt_data(data[website]["password"])
+            pyperclip.copy(password)
+            messagebox.showinfo(website, message=f"Email/Username: {email}\nPassword: {password}")
+        except FileNotFoundError:
+            messagebox.showerror("Error", "Data file not found.")
+        except KeyError:
+            messagebox.showwarning("Error", "No details for the website exists.")
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -24,7 +71,6 @@ def password_generator():
 
     random.shuffle(password_list)
     password = "".join(password_list)
-    print(f"Your password is: {password}")
     pass_entry.delete(0, "end")
     pass_entry.insert(0, password)
     pyperclip.copy(password)
@@ -36,13 +82,28 @@ def save_password():
     username = email_entry.get()
     password = pass_entry.get()
 
-    if website and username and password:
-        with open("pass.txt", "a") as pass_file:
-            pass_file.write(f"{website} | {username} | {password}\n")
+    new_data = {
+        website: {
+            "email": encrypt_data(username),
+            "password": encrypt_data(password),
+        }
+    }
 
-        website_entry.delete(first=0, last="end")
-        email_entry.delete(first=0, last="end")
-        pass_entry.delete(first=0, last="end")
+    if website and username and password:
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4, sort_keys=True)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4, sort_keys=True)
+        finally:
+            website_entry.delete(first=0, last="end")
+            email_entry.delete(first=0, last="end")
+            pass_entry.delete(first=0, last="end")
     else:
         messagebox.showwarning(title="Warning", message="Please don't leave any fields empty!")
 
@@ -58,23 +119,26 @@ canvas.grid(row=0, column=1)
 
 website_label = Label(text="Website: ", font=("arial", 10, "normal"))
 website_label.grid(row=1, column=0)
-website_entry = Entry(width=45, font=("arial", 10, "normal"))
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=30, font=("arial", 10, "normal"))
+website_entry.grid(row=1, column=1)
 
 email_label = Label(text="Email/Username: ", font=("arial", 10, "normal"))
 email_label.grid(row=2, column=0)
 email_entry = Entry(width=45, font=("arial", 10, "normal"))
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.grid(row=2, column=1, columnspan=2, sticky="WE")
 
 pass_label = Label(text="Password: ", font=("arial", 10, "normal"))
 pass_label.grid(row=3, column=0)
-pass_entry = Entry(width=29, font=("arial", 10, "normal"))
+pass_entry = Entry(width=30, font=("arial", 10, "normal"))
 pass_entry.grid(row=3, column=1)
 
-generate_button = Button(text="Generate Password", command=password_generator)
+generate_button = Button(text="Generate Password", width=14, command=password_generator)
 generate_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=44, command=save_password)
+add_button = Button(text="Add", width=45, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2)
+
+search_button = Button(text="Search", width=14, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
